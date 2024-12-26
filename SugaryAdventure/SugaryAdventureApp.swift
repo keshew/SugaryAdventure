585a6750b3ffda9c6322b9d1c8b93f7288fbe7c0
keshew <artyom.korotckov2017@yandex.ru>
2024-12-26 22:15:47 +0500

    Initial Commit

---
+++ SugaryAdventure/SugaryAdventureApp.swift
@@ -0,0 +1,17 @@
+//
+//  SugaryAdventureApp.swift
+//  SugaryAdventure
+//
+//  Created by Артём Коротков on 26.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct SugaryAdventureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
